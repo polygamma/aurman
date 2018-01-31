0ef2880b987099c090604b2d5ed421229e6ce3a0
polygamma <jonny.westphalen@googlemail.com>
2018-01-31 07:37:48 +0100

    dirty try, except.

--- src/Main.py
+++ src/Main.py
@@ -1,8 +1,12 @@
 import Utils
 import logging
 import sys
+import Exceptions
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(module)s - %(funcName)s - %(levelname)s - %(message)s')
 
 if __name__ == '__main__':
-    Utils.install_packages(sys.argv[1:])
+    try:
+        Utils.install_packages(sys.argv[1:])
+    except (Exceptions.InvalidInput, Exceptions.ConnectionProblem):
+        pass
