cdbe203d6d174ce02bddee4214f9c06d231560b4
polygamma <jonny.westphalen@googlemail.com>
2018-02-14 16:09:06 +0100

    refactor beginning

--- src/classes.py
+++ src/classes.py
@@ -1,9 +1,11 @@
 from typing import Sequence, List, Tuple, Union
 from enum import Enum, auto
 from copy import deepcopy
-from wrappers import expac
 from aur_utilities import is_devel, get_aur_info
-from system_class import System
+from utilities import strip_versioning_from_name, split_name_with_versioning, version_comparison
+import logging
+from own_exceptions import InvalidInput
+from wrappers import expac, pacman
 
 
 class PossibleTypes(Enum):
@@ -247,3 +249,218 @@ class Package:
             solution_tuple[0].append(self)
 
         return solution_visited_list
+
+
+class System:
+    __groups_names = None
+
+    @staticmethod
+    def get_groups_names():
+        if System.__groups_names is None:
+            System.__groups_names = pacman("-Sg", True, sudo=False)
+
+        return System.__groups_names
+
+    @staticmethod
+    def get_installed_packages() -> List['Package']:
+        """
+        Returns the installed packages on the system
+
+        :return:    A list containing the installed packages
+        """
+        repo_packages_names = set(expac("-S", ('n',), ()))
+        installed_packages_names = set(expac("-Q", ('n',), ()))
+        installed_repo_packages_names = installed_packages_names & repo_packages_names
+        unclassified_installed_names = installed_packages_names - installed_repo_packages_names
+
+        return_list = []
+
+        # installed repo packages
+        return_list.extend(
+            Package.get_packages_from_expac("-Q", list(installed_repo_packages_names), PossibleTypes.REPO_PACKAGE))
+
+        # installed aur packages
+        installed_aur_packages_names = set(
+            [package.name for package in Package.get_packages_from_aur(list(unclassified_installed_names))])
+        return_list.extend(
+            Package.get_packages_from_expac("-Q", list(installed_aur_packages_names), PossibleTypes.AUR_PACKAGE))
+        unclassified_installed_names -= installed_aur_packages_names
+
+        # installed not repo not aur packages
+        return_list.extend(Package.get_packages_from_expac("-Q", list(unclassified_installed_names),
+                                                           PossibleTypes.PACKAGE_NOT_REPO_NOT_AUR))
+
+        return return_list
+
+    @staticmethod
+    def get_repo_packages() -> List['Package']:
+        """
+        Returns the current repo packages.
+
+        :return:    A list containing the current repo packages
+        """
+        return Package.get_packages_from_expac("-S", (), PossibleTypes.REPO_PACKAGE)
+
+    def __init__(self, packages: Sequence['Package']):
+        self.all_packages_dict = {}  # names as keys and packages as values
+        self.repo_packages_list = []  # list containing the repo packages
+        self.aur_packages_list = []  # list containing the aur but not devel packages
+        self.devel_packages_list = []  # list containing the aur devel packages
+        self.not_repo_not_aur_packages_list = []  # list containing the packages that are neither repo nor aur packages
+
+        # reverse dict for finding providings. names of providings as keys and providing packages as values in lists
+        self.provides_dict = {}
+        # same for conflicts
+        self.conflicts_dict = {}
+
+        self.append_packages(packages)
+
+    def append_packages(self, packages: Sequence['Package']):
+        """
+        Appends packages to this system.
+
+        :param packages:    The packages to append in a sequence
+        """
+        for package in packages:
+            if package.name in self.all_packages_dict:
+                logging.error("Package {} already known".format(package))
+                raise InvalidInput()
+
+            self.all_packages_dict[package.name] = package
+
+            if package.type_of is PossibleTypes.REPO_PACKAGE:
+                self.repo_packages_list.append(package)
+            elif package.type_of is PossibleTypes.AUR_PACKAGE:
+                self.aur_packages_list.append(package)
+            elif package.type_of is PossibleTypes.DEVEL_PACKAGE:
+                self.devel_packages_list.append(package)
+            else:
+                assert package.type_of is PossibleTypes.PACKAGE_NOT_REPO_NOT_AUR
+                self.not_repo_not_aur_packages_list.append(package)
+
+        self.__append_to_x_dict(packages, 'provides')
+        self.__append_to_x_dict(packages, 'conflicts')
+
+    def __append_to_x_dict(self, packages: Sequence['Package'], dict_name: str):
+        dict_to_append_to = getattr(self, "{}_dict".format(dict_name))
+
+        for package in packages:
+            relevant_package_values = getattr(package, dict_name)
+
+            for relevant_value in relevant_package_values:
+                value_name = strip_versioning_from_name(relevant_value)
+                if value_name in dict_to_append_to:
+                    dict_to_append_to[value_name].append(package)
+                else:
+                    dict_to_append_to[value_name] = [package]
+
+    def provided_by(self, dep: str) -> List['Package']:
+        """
+        Providers for the dep
+
+        :param dep:     The dep to be provided
+        :return:        List containing the providing packages
+        """
+
+        dep_name, dep_cmp, dep_version = split_name_with_versioning(dep)
+        return_list = []
+
+        if dep_name in self.all_packages_dict:
+            package = self.all_packages_dict[dep_name]
+            if dep_cmp == "":
+                return_list.append(package)
+            elif version_comparison(package.version, dep_cmp, dep_version):
+                return_list.append(package)
+
+        if dep_name in self.provides_dict:
+            possible_packages = self.provides_dict[dep_name]
+            for package in possible_packages:
+
+                if package in return_list:
+                    continue
+
+                for provide in package.provides:
+                    provide_name, provide_cmp, provide_version = split_name_with_versioning(provide)
+
+                    if provide_name != dep_name:
+                        continue
+
+                    if dep_cmp == "":
+                        return_list.append(package)
+                    elif (provide_cmp == "=" or provide_cmp == "==") and version_comparison(provide_version, dep_cmp,
+                                                                                            dep_version):
+                        return_list.append(package)
+                    elif (provide_cmp == "") and version_comparison(package.version, dep_cmp, dep_version):
+                        return_list.append(package)
+
+        return return_list
+
+    def conflicting_with(self, package: 'Package') -> List['Package']:
+        """
+        Returns the packages conflicting with "package"
+
+        :param package:     The package to check for conflicts with
+        :return:            List containing the conflicting packages
+        """
+        name = package.name
+        version = package.version
+
+        return_list = []
+
+        if name in self.all_packages_dict:
+            possible_conflict_package = self.all_packages_dict[name]
+            if version != possible_conflict_package.version:
+                return_list.append(possible_conflict_package)
+
+        if name in self.conflicts_dict:
+            possible_conflict_packages = self.conflicts_dict[name]
+            for possible_conflict_package in possible_conflict_packages:
+
+                if possible_conflict_package in return_list:
+                    continue
+
+                for conflict in possible_conflict_package.conflicts:
+                    conflict_name, conflict_cmp, conflict_version = split_name_with_versioning(conflict)
+
+                    if conflict_name != name:
+                        continue
+
+                    if conflict_cmp == "":
+                        return_list.append(possible_conflict_package)
+                    elif version_comparison(version, conflict_cmp, conflict_version):
+                        return_list.append(possible_conflict_package)
+
+        return return_list
+
+    def append_packages_by_name(self, packages_names: Sequence[str], installed_system: 'System',
+                                only_unfulfilled_deps: bool):
+        """
+        Appends packages to this system by names.
+
+        :param packages_names:          The names of the packages
+        :param installed_system:        The system containing the already installed packages
+        :param only_unfulfilled_deps:   True (default) if one only wants to fetch unfulfilled deps packages, False otherwise
+        """
+
+        packages_names = set([strip_versioning_from_name(name) for name in packages_names])
+        packages_names_to_fetch = [name for name in packages_names if name not in self.all_packages_dict]
+
+        while packages_names_to_fetch:
+            fetched_packages = Package.get_packages_from_aur(packages_names_to_fetch)
+            self.append_packages(fetched_packages)
+
+            deps_of_the_fetched_packages = []
+            for package in fetched_packages:
+                deps_of_the_fetched_packages.extend(package.relevant_deps())
+
+            if only_unfulfilled_deps:
+                relevant_deps = []
+                for dep in deps_of_the_fetched_packages:
+                    if not installed_system.provided_by(dep):
+                        stripped_dep = strip_versioning_from_name(dep)
+                        if stripped_dep not in relevant_deps:
+                            relevant_deps.append(stripped_dep)
+            else:
+                relevant_deps = list(set([strip_versioning_from_name(dep) for dep in deps_of_the_fetched_packages]))
+
+            packages_names_to_fetch = [dep for dep in relevant_deps if dep not in self.all_packages_dict]
