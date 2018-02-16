7d03f5cf6dbcd9cc3ce3daf6d15ae639d274e013
polygamma <jonny.westphalen@googlemail.com>
2018-02-16 13:26:34 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+"""aurman AUR helper
+
+see: https://github.com/polygamma/aurman
+"""
+
+from codecs import open
+from os import path
+
+from setuptools import setup, find_packages
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='aurman',
+
+    version='1.0a.5',  # do not forget to change this
+
+    description='Arch Linux AUR helper',
+
+    long_description=long_description,
+
+    url='https://github.com/polygamma/aurman',
+
+    author='Jonni Westphalen',
+
+    author_email='jonny.westphalen@googlemail.com',
+
+    packages=find_packages('src', exclude=['unit_tests']),
+    package_dir={'': 'src'},
+
+    entry_points={
+        'console_scripts': [
+            'aurman=aurman.main:main',
+        ]
+    }
+)
