1f57a9386811a8b61f1fc42dfd027c6c3adca5bc
polygamma <jonny.westphalen@googlemail.com>
2018-02-24 09:35:20 +0100

    version bump and change pkgbuild

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='aurman',
 
-    version='1.0',  # do not forget to change this
+    version='1.0.2.r0.g2893824',  # do not forget to change this
 
     description='Arch Linux AUR helper',
 
