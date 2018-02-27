ba8f70f9522de4c791b341bdcc6fd8dc46fde71b
polygamma <jonny.westphalen@googlemail.com>
2018-02-27 15:56:22 +0100

    beginning of new help

--- src/aurman/help_printing.py
+++ src/aurman/help_printing.py
@@ -1,4 +1,4 @@
-from typing import Iterable, Any, List
+from typing import Any, List
 
 from aurman.coloring import Colors
 
@@ -6,21 +6,22 @@ from aurman.coloring import Colors
 class HelpPoint:
     def __init__(self, head, children):
         self.head: Any = head
-        self.children: Iterable = children
+        self.children: List = children
 
     def __repr__(self):
-        return "{}{}".format(Colors.BOLD(self.head), ''.join(
-            ["\n   {} {}".format(Colors.BOLD(Colors.LIGHT_CYAN("-")), child) for child in self.children]))
+        return "{}\n{}".format(Colors.BOLD(self.head), ''.join(["\n    {}".format(child) for child in self.children]))
 
 
 class HelpOption:
     def __init__(self, names, description):
-        self.names: Iterable[str] = names
+        self.names: List[str] = names
         self.description: str = description
 
     def __repr__(self):
         if self.description:
-            return "{}: {}".format(", ".join([Colors.LIGHT_GREEN(name) for name in self.names]), self.description)
+            return_beginning = ", ".join([Colors.LIGHT_GREEN(name) for name in self.names]).ljust(
+                20 + 10 * len(self.names))
+            return "{}:  {}".format(return_beginning, self.description)
         else:
             return "{}".format(", ".join([Colors.LIGHT_GREEN(name) for name in self.names]))
 
@@ -38,13 +39,13 @@ aurman_help = Help([])
 
 # usage
 usage_title = "Usage"
-usage = "aurman < operation > [ options ] [ targets ] - see also https://www.archlinux.org/pacman/pacman.8.html"
+usage = "aurman <operation> [options] [targets]\n    see also https://www.archlinux.org/pacman/pacman.8.html"
 aurman_help.points.append(HelpPoint(usage_title, (usage,)))
 
 # description
 description_title = "Description"
 description = "aurman is meant as a {}. " \
-              "All pacman operations are supported, " \
+              "All pacman operations are supported,\n    " \
               "but calling aurman with an operation besides " \
               "{} or {} will {}.".format(Colors.BOLD("pacman wrapper"),
                                          Colors.BOLD(Colors.LIGHT_GREEN("--sync")),
@@ -58,23 +59,43 @@ native = "the following native pacman options for " \
 native_points = []
 aurman_help.points.append(HelpPoint(native, native_points))
 
-native_points.append(HelpOption(["-r", "--root"], ""))
-native_points.append(HelpOption(["-v", "--verbose"], ""))
-native_points.append(HelpOption(["--cachedir"], ""))
-native_points.append(HelpOption(["--color"], ""))
-native_points.append(HelpOption(["--debug"], ""))
-native_points.append(HelpOption(["--gpgdir"], ""))
-native_points.append(HelpOption(["--hookdir"], ""))
-native_points.append(HelpOption(["--logfile"], ""))
-native_points.append(HelpOption(["--noconfirm"], ""))
-native_points.append(HelpOption(["--confirm"], ""))
-native_points.append(HelpOption(["--force"], ""))
-native_points.append(HelpOption(["--asdeps"], ""))
-native_points.append(HelpOption(["--asexplicit"], ""))
-native_points.append(HelpOption(["--needed"], ""))
-native_points.append(HelpOption(["-s", "--search"], ""))
-native_points.append(HelpOption(["-u", "--sysupgrade"], ""))
-native_points.append(HelpOption(["-y", "--refresh"], ""))
+native_points.append(HelpOption(["-r", "--root"],
+                                "Specify an alternative database location (a typical default is /var/lib/pacman)"))
+native_points.append(HelpOption(["-v", "--verbose"],
+                                "Output paths such as as the Root, Conf File, DB Path, Cache Dirs, etc."))
+native_points.append(HelpOption(["--cachedir"],
+                                "Specify an alternative package cache location "
+                                "(a typical default is /var/cache/pacman/pkg)"))
+native_points.append(HelpOption(["--color"],
+                                "Specify when to enable coloring"))
+native_points.append(HelpOption(["--debug"],
+                                "Display debug messages"))
+native_points.append(HelpOption(["--gpgdir"],
+                                "Specify a directory of files used by GnuPG to verify package signatures "
+                                "(default is /etc/pacman.d/gnupg)"))
+native_points.append(HelpOption(["--hookdir"],
+                                "Specify a alternative directory containing hook files "
+                                "(default is /etc/pacman.d/hooks)"))
+native_points.append(HelpOption(["--logfile"],
+                                "Specify an alternate log file"))
+native_points.append(HelpOption(["--noconfirm"],
+                                "Bypass any and all 'Are you sure?' messages"))
+native_points.append(HelpOption(["--confirm"],
+                                "Cancels the effects of a previous --noconfirm"))
+native_points.append(HelpOption(["--force"],
+                                "Bypass file conflict checks and overwrite conflicting files"))
+native_points.append(HelpOption(["--asdeps"],
+                                "Install packages non-explicitly"))
+native_points.append(HelpOption(["--asexplicit"],
+                                "Install packages explicitly"))
+native_points.append(HelpOption(["--needed"],
+                                "Do not reinstall the targets that are already up-to-date"))
+native_points.append(HelpOption(["-s", "--search"],
+                                "This will search each package in the sync databases for names or descriptions"))
+native_points.append(HelpOption(["-u", "--sysupgrade"],
+                                "Upgrades all packages that are out-of-date"))
+native_points.append(HelpOption(["-y", "--refresh"],
+                                "Download a fresh copy of the master package database from the server"))
 
 # native pacman options for aurman
 native_aurman = "the following native pacman options for " \
@@ -83,10 +104,14 @@ native_aurman = "the following native pacman options for " \
 native_aurman_points = []
 aurman_help.points.append(HelpPoint(native_aurman, native_aurman_points))
 
-native_aurman_points.append(HelpOption(["--noconfirm"], ""))
-native_aurman_points.append(HelpOption(["--needed"], ""))
-native_aurman_points.append(HelpOption(["-s", "--search"], ""))
-native_aurman_points.append(HelpOption(["-u", "--sysupgrade"], ""))
+native_aurman_points.append(HelpOption(["--noconfirm"],
+                                       "Bypass any and all 'Are you sure?' messages"))
+native_aurman_points.append(HelpOption(["--needed"],
+                                       "Do not reinstall the targets that are already up-to-date"))
+native_aurman_points.append(HelpOption(["-s", "--search"],
+                                       "This will search each package in the sync databases for names or descriptions"))
+native_aurman_points.append(HelpOption(["-u", "--sysupgrade"],
+                                       "Upgrades all packages that are out-of-date"))
 
 # aurman exclusive options
 only_aurman = "aurman exclusive options for {} or {}".format(Colors.LIGHT_GREEN("--sync"), Colors.LIGHT_GREEN("-S"))
