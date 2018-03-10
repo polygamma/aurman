047850a7c97ede1d969ff7c0773a3e61b672c709
polygamma <jonny.westphalen@googlemail.com>
2018-03-10 22:15:59 +0100

    Add more docker tests

--- src/docker_tests/docker_tests.sh
+++ src/docker_tests/docker_tests.sh
@@ -31,7 +31,24 @@ test_command aurman -S cower --noconfirm --noedit --pgp_fetch --keyserver hkp://
 # check if cower installed
 test_command pacman -Qi cower
 
-# search solution to install mingw-w64-gcc
+# install cower-git
+test_command yes | aurman -S cower-git --noconfirm --noedit --pgp_fetch --keyserver hkp://ipv4.pool.sks-keyservers.net:11371
+
+# check if cower-git installed
+test_command pacman -Qi cower-git
+
+# search solutions to install mingw-w64-gcc
 test_command aurmansolver -S mingw-w64-gcc
+test_command aurmansolver -S mingw-w64-gcc --deep_search
+
+# search solutions to install ros-indigo-desktop-full
+test_command aurmansolver -S ros-indigo-desktop-full
+test_command aurmansolver -S ros-indigo-desktop-full --deep_search
+
+# install pacman-git
+test_command yes | aurman -S pacman-git --noconfirm --noedit --pgp_fetch --keyserver hkp://ipv4.pool.sks-keyservers.net:11371
+
+# check if pacman-git installed
+test_command pacman -Qi pacman-git
 
 if ${ANY_FAILED}; then exit 1; else exit 0; fi
