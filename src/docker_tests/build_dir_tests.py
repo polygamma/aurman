88ff876249d230801e92cec61dbb1d4fa4592e01
polygamma <jonny.westphalen@googlemail.com>
2018-03-12 21:49:58 +0100

    Add new docker tests
    test building in custom builddir

---
+++ src/docker_tests/build_dir_tests.py
@@ -0,0 +1,60 @@
+import os
+from subprocess import run, DEVNULL
+from sys import exit
+
+
+class CurrentTest:
+    to_return: int = 0
+
+
+def test_command(command: str, dir_to_execute: str = None):
+    if dir_to_execute is None:
+        return_code = run(command, shell=True, stdout=DEVNULL, stderr=DEVNULL).returncode
+    else:
+        return_code = run(command, shell=True, stdout=DEVNULL, stderr=DEVNULL, cwd=dir_to_execute).returncode
+
+    if return_code == 0:
+        print("Success with: '{}'".format(command))
+    else:
+        print("Error with: '{}'".format(command))
+
+    if CurrentTest.to_return == 0 and return_code != 0:
+        CurrentTest.to_return = 1
+
+
+if __name__ == '__main__':
+    # install aurman
+    test_command("sudo python setup.py install --optimize=1", "/home/aurman/aurman-git")
+
+    # change build dir
+    test_command("mkdir -p /home/aurman/build_dir")
+    test_command('sudo sh -c "{}"'
+                 ''.format("echo 'BUILDDIR=/home/aurman/build_dir' >> /etc/makepkg.conf"))
+
+    # install cower
+    test_command("yes | aurman -S cower --noedit --pgp_fetch --keyserver hkp://ipv4.pool.sks-keyservers.net:11371")
+    # check if cower installed
+    test_command("pacman -Qi cower")
+
+    if "cower" not in os.listdir("/home/aurman/build_dir"):
+        print("Error: cower has not been built in build_dir")
+    else:
+        print("Success: cower has been built in build_dir")
+
+    # install pacman-git
+    test_command("yes | aurman -S pacman-git --noedit --pgp_fetch --keyserver hkp://ipv4.pool.sks-keyservers.net:11371")
+
+    # check if pacman-git installed
+    test_command("pacman -Qi pacman-git")
+
+    # install cower-git
+    test_command("yes | aurman -S cower-git --noedit --pgp_fetch --keyserver hkp://ipv4.pool.sks-keyservers.net:11371")
+    # check if cower-git installed
+    test_command("pacman -Qi cower-git")
+
+    if "cower-git" not in os.listdir("/home/aurman/build_dir"):
+        print("Error: cower-git has not been built in build_dir")
+    else:
+        print("Success: cower-git has been built in build_dir")
+
+    exit(CurrentTest.to_return)
