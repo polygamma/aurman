b2ab01ad40402176941421fa9907237bf9e68a17
polygamma <jonny.westphalen@googlemail.com>
2018-03-12 22:24:45 +0100

    Add docker test for testing repo
    do this update with --do_everything to test that

---
+++ src/docker_tests/testing_repo_tests.py
@@ -0,0 +1,42 @@
+from subprocess import run, DEVNULL
+from sys import exit
+
+
+class CurrentTest:
+    to_return: int = 0
+
+
+def test_command(command: str, dir_to_execute: str = None):
+    if dir_to_execute is None:
+        return_code = run(command, shell=True, stdout=DEVNULL, stderr=DEVNULL).returncode
+    else:
+        return_code = run(command, shell=True, stdout=DEVNULL, stderr=DEVNULL, cwd=dir_to_execute).returncode
+
+    if return_code == 0:
+        print("Success with: '{}'".format(command))
+    else:
+        print("Error with: '{}'".format(command))
+
+    if CurrentTest.to_return == 0 and return_code != 0:
+        CurrentTest.to_return = 1
+
+
+if __name__ == '__main__':
+    # install aurman
+    test_command("sudo python setup.py install --optimize=1", "/home/aurman/aurman-git")
+
+    # activate testing
+    test_command('sudo sh -c "{}"'
+                 ''.format("sed -i '/\[testing\]/,/Include/s/^[ ]*#//' /etc/pacman.conf"))
+
+    # update with aurman --do_everything
+    test_command("yes | aurman -Syu --do_everything"
+                 " --noedit --pgp_fetch --keyserver hkp://ipv4.pool.sks-keyservers.net:11371")
+
+    if run("pacman -Qqun", shell=True, stdout=DEVNULL, stderr=DEVNULL).returncode != 0:
+        print("Success: everything known has been updated")
+    else:
+        print("Error: there are some things which are not updated")
+        CurrentTest.to_return = 1
+
+    exit(CurrentTest.to_return)
