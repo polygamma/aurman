1247ebe0f96a3a6a18760e40ecf6101331b6106d
Jonni Westphalen <jonny.westphalen@googlemail.com>
2018-05-25 01:28:58 +0200

    version bump to 2.9.28

--- PKGBUILD_NON_DEVEL
+++ PKGBUILD_NON_DEVEL
@@ -1,6 +1,6 @@
 # Maintainer: Jonni Westphalen <jonny.westphalen@googlemail.com>
 pkgname=aurman
-pkgver=2.9.27
+pkgver=2.9.28
 pkgrel=1
 pkgdesc="aurman AUR helper with almost pacman syntax"
 arch=('any')
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='aurman',
 
-    version='2.9.27',  # do not forget to change this
+    version='2.9.28',  # do not forget to change this
 
     python_requires='>=3.6',
 
