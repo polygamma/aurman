04f5c2fee21d2f8fdd15fea0316a76494db8aa16
Jonni Westphalen <jonny.westphalen@googlemail.com>
2018-06-08 22:37:13 +0200

    make use of reflector in docker tests

--- src/docker_tests/build_dir_tests.py
+++ src/docker_tests/build_dir_tests.py
@@ -23,6 +23,11 @@ def test_command(command: str, dir_to_execute: str = None):
 
 
 if __name__ == '__main__':
+    # install expac-git
+    test_command("git clone https://aur.archlinux.org/expac-git.git")
+    test_command("makepkg -si --needed --noconfirm", dir_to_execute="/home/aurman/expac-git")
+    test_command("rm -rf expac-git/")
+
     # install aurman
     test_command("sudo python setup.py install --optimize=1", "/home/aurman/aurman-git")
 
