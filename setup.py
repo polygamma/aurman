ef47d7b5ea8816d681774ba69ea2e0021c960f41
Jonni Westphalen <jonny.westphalen@googlemail.com>
2018-07-29 21:54:15 +0200

    version bump to 2.17

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='aurman',
 
-    version='2.16.11',  # do not forget to change this
+    version='2.17',  # do not forget to change this
 
     python_requires='>=3.6',
 
